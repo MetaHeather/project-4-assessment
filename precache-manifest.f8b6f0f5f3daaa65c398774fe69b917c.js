f4eaa04e8d8f81444d97aecad4d902bd4608afb6
Heather Etheridge <heathernetheridge@gmail.com>
2019-10-09 11:40:11 -0500

    Updates

---
+++ precache-manifest.f8b6f0f5f3daaa65c398774fe69b917c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf115556b914d59dc6b0ed12e609243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d9064ae78941917e85",
+    "url": "/static/css/main.2fc486e6.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "72d9064ae78941917e85",
+    "url": "/static/js/main.e7c1c7cc.chunk.js"
+  },
+  {
+    "revision": "24d44acec74bb9c80628",
+    "url": "/static/js/runtime-main.fa43bd8c.js"
+  }
+]);
